521933a38a99e103b6f57663fe39ea9ce683a208
Thomas Davidson <tomd1969@gmail.com>
2022-10-04 23:18:16 -0400

    Added background.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+//get how many times we need to iterate the sky and grass
+
+const blocksWide = window.innerWidth / 100;
+const blocksHigh = window.innerHeight / 100;
+
+
+//The height of the sky is 1/3 the height, with the ground taking up the other 1/3.
+
+const skyHeight = blocksHigh / 3;
+const groundHeight = blocksHigh - skyHeight;
+
+function addBackground(asset, startingY, endingY) {
+    for(let x = 0; x < blocksWide; x++) {
+        for(let y = startingY; y < endingY; y++){
+            insertSprite(asset, x * 100, y * 100);
+        }
+    }
+}
+
+addBackground('assets/grass.png', 0, groundHeight - 1);
+addBackground('assets/sky.png', groundHeight - 1, blocksHigh)
+
 // let greenCharacter = document.createElement('img')
 // greenCharacter.src = 'assets/green-character.gif'
 // greenCharacter.style.position = 'fixed'
@@ -30,13 +52,13 @@ function newItem(asset, posLeft, posBottom) {
     return item;
 }
 
-let character = insertSprite('assets/green-character.gif', 100, 100);
-let pineTree = insertSprite('assets/pine-tree.png', 450, 200);
-let Tree = insertSprite('assets/tree.png', 200, 300);
-let pillar = insertSprite('assets/pillar.png', 350, 100);
-let crate = insertSprite('assets/crate.png', 150, 200);
-let well = insertSprite('assets/well.png', 500, 425);
-let sword = newItem('assets/sword.png', 500, 405);
-let shield = newItem('assets/shield.png', 165, 185);
-let staff = newItem('assets/staff.png', 600, 100);
+const character = insertSprite('assets/green-character.gif', 100, 100);
+const pineTree = insertSprite('assets/pine-tree.png', 450, 200);
+const Tree = insertSprite('assets/tree.png', 200, 300);
+const pillar = insertSprite('assets/pillar.png', 350, 100);
+const crate = insertSprite('assets/crate.png', 150, 200);
+const well = insertSprite('assets/well.png', 500, 425);
+const sword = newItem('assets/sword.png', 500, 405);
+const shield = newItem('assets/shield.png', 165, 185);
+const staff = newItem('assets/staff.png', 600, 100);
 
